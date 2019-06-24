d1e0710126025b3d53b17262380156d216a5eeb9
Wasin Watthanasrisong <wasinwatt@gmail.com>
2019-06-24 23:52:12 +0700

    use template_cloudinit_config instead of userdata file

--- simple-hello-world/main.tf
+++ simple-hello-world/main.tf
@@ -22,11 +22,29 @@ resource "aws_security_group" "allow_8000" {
   }
 }
 
+data "template_cloudinit_config" "ec2userdata" {
+  base64_encode = true
+  gzip          = false
+
+  part {
+    content = <<EOF
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y python
+echo "This is fine"
+echo "Hello, world! <img src=\"https://${aws_s3_bucket.public-bucket.bucket}.s3-${aws_s3_bucket.public-bucket.region}.amazonaws.com/procastination-map.jpg\">" > index.html
+python -m SimpleHTTPServer 8000
+EOF
+
+    content_type = "text/x-shellscript"
+  }
+}
+
 resource "aws_instance" "hello-world" {
   ami           = "ami-0b9eda9b936098dfd"
   instance_type = "t2.micro"
   security_groups = [aws_security_group.allow_8000.name]
-  user_data = file("userdata.sh")
+  user_data = data.template_cloudinit_config.ec2userdata.rendered
 }
 
 resource "aws_s3_bucket" "public-bucket" {
